427bd7b4b478081873aebdd44290c364f3db1b3f
vanesairrazabal <vaneeirra@gmail.com>
2022-08-20 20:51:01 -0300

    Nuevas Funcionalidades
    Se trabaja con el index.htm, products.js, index1.html, products-info.html

--- js/login.js
+++ js/login.js
@@ -1,6 +1,31 @@
 
-/*           
-let mail= document.getElementById('login').email.value;
-console.log("mail:" + mail)
-*/
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("formLogin").addEventListener('submit', validarFormulario); 
+});
 
+function validarFormulario(evento) {
+ 
+  let mail= document.getElementById('mi-email').value;
+  let password= document.getElementById('mi-contraseña').value;
+  let error= document.getElementById('error')
+  let errorMail = 'No has escrito nada en el email'
+  let errorPass = 'La contraseña no es válida'
+  let mensajesDeError= [];
+  evento.preventDefault();
+
+  if(mail === "") {
+    mensajesDeError.push(errorMail);
+  }
+
+  if (password.length < 6) {
+    mensajesDeError.push(errorPass);
+  }
+
+  if (mensajesDeError.length > 0) {
+    error.style.color = 'red';
+    error.innerHTML = '<p>' + mensajesDeError.join(' , ') + '</p>'
+  }
+  else {
+    window.location.href = 'index1.html';
+  }
+}
--- js/products.js
+++ js/products.js
@@ -4,11 +4,12 @@ let maxCount = undefined;
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-    console.log(PRODUCTS_URL+'101.json')
-    getJSONData(PRODUCTS_URL+'101.json').then(function(resultObj){
+    
+    let idCategoria = localStorage.getItem("catID")
+    getJSONData(PRODUCTS_URL + idCategoria + '.json').then(function(resultObj){
         if (resultObj.status === "ok"){
             currentProductsArray = resultObj.data.products
-            showProductsList()
+            showProductsList(idCategoria)
         }
     });
     
@@ -19,37 +20,63 @@ function setProdID(id) {
     window.location = "product-info.html"
 }
 
-function showProductsList(){
+function showProductsList(idCategoria){
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentProductsArray.length; i++){
-        let product = currentProductsArray[i];
 
-        //if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
-            //((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){
+    if(idCategoria==='101'){
 
-            htmlContentToAppend += `
-            <div onclick="setProdID(${product.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${product.name}</h4>
-                            <small class="text-muted">${product.soldCount} vendidos</small>
+        for(let i = 0; i < currentProductsArray.length; i++){
+            let product = currentProductsArray[i];
+
+            //if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
+                //((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){
+
+                    htmlContentToAppend += `
+                    <div onclick="setProdID(${product.id})" class="list-group-item list-group-item-action cursor-active">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                            </div>
+                            <div class="col">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h4 class="mb-1">${product.name}</h4>
+                                    <small class="text-muted">${product.soldCount} vendidos</small>
+                                </div>
+                                <p class="mb-1">${product.description}</p>
+                                <div class="d-flex w-100 justify-content-between">
+                                    <small class="text-muted"></small>
+                                    <h4 class="mb-1">${product.currency} ${product.cost}</h4>
+                                </div>
+                            </div>
                         </div>
-                        <p class="mb-1">${product.description}</p>
-                        <div class="d-flex w-100 justify-content-between">
-                            <small class="text-muted"></small>
-                            <h4 class="mb-1">${product.currency} ${product.cost}</h4>
-                        </div>
                     </div>
-                </div>
-            </div>
-            `
-        //}
+                    `
+                
+                
+                /*
+                htmlContentToAppend += `
+                <div class="col-3" float=inline>
+                            <div class="card">
+                            <div class="image">
+                                <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
+                            </div>
+                            <h2 class="title">${product.name}</h2>
+                            <h5 class="description">${product.description}</h5>
+                            <span class="price">${product.currency} ${product.cost}</span>
+                            </div>
+                        </div>
+                `*/
+            //}
 
+            document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+        }
+    }else {
+        htmlContentToAppend +=`
+        <div class="alert alert-danger text-center" role="alert">
+            <h4 class="alert-heading">Funcionalidad en desarrollo</h4>
+        </div>
+        `
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
 }
