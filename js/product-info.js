cdc4caad52c3153b513a01de17f062f546afa454
vanesairrazabal <vaneeirra@gmail.com>
2022-09-30 15:13:34 -0300

    nuevos ajustes

--- js/product-info.js
+++ js/product-info.js
@@ -26,44 +26,43 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 });
 
+function agregarProducto(){
+   location.href= 'cart.html';
+}
+
 function mostrarDetalles(prod) {
     console.log(prod);
-    let htmlContentToAppend = "";
-    /*let sliderImages = '<ul class="slider">';
-    let navImages = '<ul class="menu">';
-
-    sliderImages += loadImagesSlider(prod);
-    sliderImages += '</ul>';
-    navImages += loadNavImagesButton(prod);
-    sliderImages += '</ul>';*/
-
-    htmlContentToAppend = `
-        <div class="col cardProduct center">
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    let htmlContentToAppend = `
+        <div class="col cardProduct center shadow">
+            <div id="carouselExampleFade" class="carousel slide carousel-fade w-75 mt-4" data-bs-ride="carousel">
                 <div class="carousel-inner">
+            `
+
+    for(let i = 0; i < prod.images.length; i++){
+        let imagen = prod.images[i]
+        if(i == 0){
+            htmlContentToAppend += `
+                <div class="carousel-item active" data-bs-interval="5000">
+                    <img src="${imagen}" class="d-block w-100" alt="...">
+                </div>
                 `
-            for(let i = 0; i < prod.images.length; i++){
-                let imagen = prod.images[i]
-                if(i == 0){
-                    htmlContentToAppend += `
-                    <div class="carousel-item active data-bs-interval="5000">
-                        <img src="${imagen}" class="d-block w-75" alt="...">
-                     </div>`
-                } else {
-                    htmlContentToAppend += `
-                    <div class="carousel-item data-bs-interval="5000"">
-                        <img src="${imagen}" class="d-block w-75" alt="...">
-                     </div>`
-                }
-                }
+        } else {
+            htmlContentToAppend += `
+                <div class="carousel-item" data-bs-interval="5000">
+                    <img src="${imagen}" class="d-block w-100" alt="...">
+                </div>
+                `
+        }
+    }
 
     htmlContentToAppend += `
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-             <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
@@ -71,44 +70,24 @@ function mostrarDetalles(prod) {
         <div class="cardProduct-info">
             <p class="cardProduct-title">${prod.name}</p>
             <p class="cardProduct-body">${prod.description}</p>
-            </div>
+        </div>
         <div class="cardProduct-footer">
             <span class="cardProduct-title price">${prod.currency} ${prod.cost}</span>
-        <div class="cardProduct-button">
-            <svg class="svg-icon" viewBox="0 0 20 20">
-                <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
-                <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
-                <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
-            </svg>
+            <div class="cardProduct-button" id= "btn-carrito">
+                <svg class="svg-icon" viewBox="0 0 20 20">
+                    <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+                    <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+                    <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
+                </svg>
+            </div>
         </div>
-        </div>`
+        `
 
     document.getElementById("ficha-container").innerHTML = htmlContentToAppend;
-};
-
-function loadImagesSlider(prod) {
-    let listImages = "";
-    for(let i = 0; i < prod.images.length; i++){
-        listImages += `
-        <li id="slide${i}">
-            <img src="${prod.images[i]}"/>
-        </li>
-    `
-    }
-    return listImages;
-};
-
-function loadNavImagesButton(prod) {
-    let navButton = "";
-    for(let i = 0; i < prod.images.length; i++){
-        navButton += `
-        <li>
-            <a href="#slide${i}">${i+1}</a>
+    document.getElementById("btn-carrito").addEventListener('click', function(){
+        agregarProducto()
+    })
     
-        </li>
-    `
-    }
-    return navButton;
 };
 
 function mostrarRelacionados(productosRelacionados){    
@@ -159,33 +138,9 @@ function mostrarComentarios(comentariosProducto) {
         let comment = todosLosComentarios[i];
         let stars = '';
         stars += loadStars(comment.score);
-        console.log("description: " + comment.description)
-    
-        htmlContentToAppend += `
-        <div onclick="" class="list-group-item list-group-item-action mb-2">
-            <div class="row">
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div>
-                            ${stars} 
-                        </div>                  
-                        <small class="design_date">${comment.dateTime}</small>
-                    </div> 
-                    <p class="mb-1 text-start diseño_user">${comment.user}:</p>
-                    <h5 class="mb-1 text-start com">${comment.description}</h5>
-                </div>       
-            </div>
-        </div>
-     `
-     document.getElementById("ficha-container2").innerHTML = htmlContentToAppend;
-    };
+        //console.log("description: " + comment.description)
 
-    for(let i = 0; i < localComment.length; i++){
-        let comment = localComment[i];
-        let stars = '';
-        stars += loadStars(comment.score);
-
-        if (comment.product === localStorage.getItem("prodID")) {
+        if(comment.product == localStorage.getItem("prodID")){
             htmlContentToAppend += `
             <div onclick="" class="list-group-item list-group-item-action mb-2">
                 <div class="row">
@@ -201,10 +156,11 @@ function mostrarComentarios(comentariosProducto) {
                     </div>       
                 </div>
             </div>
-        `
-        }  
-     document.getElementById("ficha-container2").innerHTML = htmlContentToAppend;
+            `
+        }
+        document.getElementById("ficha-container2").innerHTML = htmlContentToAppend;
     };
+
 };
 
 function loadStars(score) {
